8755c4724ae9182d31f276775f037355242056c9
Igor <qwertyhock@gmail.com>
2024-02-13 09:45:47 +0300

    add exercise-log

---
+++ server/app/workout/workout.routes.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { protect } from '../middleware/auth.middleware.js'
+import {
+	createNewWorkout,
+	deleteWorkout,
+	getWorkouts,
+	updateWorkout,
+	getWorkout
+} from './workout.controller.js'
+
+const router = express.Router()
+
+router.route('/').post(protect, createNewWorkout).get(protect, getWorkouts)
+
+router
+	.route('/:id')
+	.get(protect, getWorkout)
+	.put(protect, updateWorkout)
+	.delete(protect, deleteWorkout)
+
+export default router
